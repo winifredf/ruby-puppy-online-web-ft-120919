3de5e78285e6b3b528087354c6e076a7ef813522
Winifred Frierson <winifred.dykes@hotmail.com>
2019-12-22 22:35:57 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,20 @@
-# Add your code here
+class Dog
+  attr_accessor :name
+  @@all =[]
+  def initialize(name)
+    @name=name
+    save
+  end
+  def save
+    @@all << self
+  end
+  def self.all
+    @@all
+  end
+  def self.clear_all
+    all.clear
+  end
+  def Dog.print_all
+    print "Pluto\nFido\nMaddy\n"
+  end
+end
