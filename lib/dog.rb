418adc9ddf0fa6945a2ea714fb8e493f72e3737b
Winifred Frierson <winifred.dykes@hotmail.com>
2019-12-22 22:20:11 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,28 +1,18 @@
 class Dog
-
   attr_accessor :name
-
-  @@all = []
+  
+  @@all=[]
 
   def initialize(name)
-    @name = name
+    @name=name
     @@all << self
   end
-  
-  def self.all
-    @@all.each do |dog|
-      puts  dog.name
-    end
-  end
-  def self.save
-    @@all << self
-  end
-  
+
   def self.clear_all
     @@all.clear
   end
-end
-  
-  def self.print_all
-    puts  "Pluto\nFido\nMaddy\n"
+
+  def self.all
+    @@all.each {|dog| puts "#{dog.name}"}
   end
+end
